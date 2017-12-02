6aa6d0881fb46a821297e5840768ba888808d6d4
PUNISHER <punisher-corp@yandex.ru>
2017-12-02 20:43:47 +0100

    fixed sidebar button

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,25 @@
 /*Menu toogle*/
 
 $( "a#menu-toggle" ).click(function() {
-    
-    $( "#sidebar-wrapper").toggleClass("hide");
-    $(".hide-md").toggleClass("hide-md");
-    $( "#page-content-wrapper").toggleClass("pl-none");
-    $( ".logo").toggleClass("hide");
-    $( ".left-part").toggleClass("show-logo");
+    if ($(window).width() <= 767){
+        if ( $("#sidebar-wrapper").hasClass("hide-md") ) {
+            $('#sidebar-wrapper').removeClass('hide-md');
+
+        } 
+        else{
+            $( "#sidebar-wrapper").toggleClass("hide");
+            $(".hide-md").toggleClass("hide-md");
+            $( "#page-content-wrapper").toggleClass("pl-none");
+            $( ".logo").toggleClass("hide");
+            $( ".left-part").toggleClass("show-logo");   
+        }
+    }
+    else{
+        $( "#sidebar-wrapper").toggleClass("hide");
+        $(".hide-md").toggleClass("hide-md");
+        $( "#page-content-wrapper").toggleClass("pl-none");
+        $( ".logo").toggleClass("hide");
+        $( ".left-part").toggleClass("show-logo");   
+
+    }
 });
